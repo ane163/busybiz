28119a5fb650b5ad15e1fc16dc7a3edb8c874db3
ane163 <amugadza180@gmail.com>
2026-09-22 21:05:20 +0530

    Update EditListing.jsx

--- frontend/src/pages/Marketplace/EditListing.jsx
+++ frontend/src/pages/Marketplace/EditListing.jsx
@@ -56,7 +56,7 @@ function EditListing() {
   // API
   // =====================================================
 
-  const API_BASE_URL = "http://localhost:5000/api";
+  const API_BASE_URL = "https://busybiz-5.onrender.com/api";
 
   // =====================================================
   // TOKEN
